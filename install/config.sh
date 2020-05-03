af64282520d1e64e0db663506a83b054f18679a8
Jia Wan <williamwanjia@gmail.com>
2020-05-03 14:09:53 +0200

    Push to github

--- install/config.sh
+++ install/config.sh
@@ -1,26 +1,32 @@
-# I3
+# Git-------------------------------------------------
+# git config --global user.email "williamwanjia@gmail.com"
+# git config --global user.name "Jia Wan"
+# git config --global core.editor "vim"
+
+
+
+# I3-------------------------------------------------
 
 # if [ -d $HOME/.config/i3 ];
 
 
 
-# VIM
+# VIM-------------------------------------------------
 # curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    # https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    #
+#     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+     
 # vim
 
-
-cd ~/repos
-
-if [ -d ctags ];
-then
-    cd ctags
-    git pull
-else
-    git clone https://github.com/universal-ctags/ctags.git
-    cd ctags
-fi
+# cd ~/repos
+# 
+# if [ -d ctags ];
+# then
+#     cd ctags
+#     git pull
+# else
+#     git clone https://github.com/universal-ctags/ctags.git
+#     cd ctags
+# fi
 
 # sudo apt install \
 #     gcc make \
