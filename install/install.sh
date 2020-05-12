2c6db26e213f39438d65bb12574a4875b9122f50
Jia Wan <williamwanjia@gmail.com>
2020-05-12 21:46:12 +0200

    Change I3 bar

--- install/install.sh
+++ install/install.sh
@@ -1,6 +1,9 @@
 sudo apt-get update
  
 sudo apt-get upgrade -y
+
+sudo add-apt-repository ppa:jonathonf/vim
+sudo apt update
  
 sudo apt-get install -y ubuntu-restricted-extras smplayer conky-all \
         vim-gnome disper gedit vim terminator dolphin default-jre fcitx \
