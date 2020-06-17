391edc2756a9ab00af5fe4ee788b31293f9922a6
Jia Wan <williamwanjia@gmail.com>
2020-06-17 16:47:29 +0200

    Initial version of VTK part

--- install/install_pvb_vtk.sh
+++ install/install_pvb_vtk.sh
@@ -9,7 +9,8 @@ sudo apt upgrade -y
 
 # install dependencies
 
-sudo apt install gcc cmake qt5-default tcl tk tcl-tclreadline git
+sudo apt install gcc cmake qt5-default tcl8.5 tcl8.5-dev libtcl8.5 \
+    tk8.5 tk8.5-dev libtk8.5 tcl-tclreadline git
 
 # make repos dir
 cd
@@ -23,8 +24,34 @@ fi
 cd repos
 
 # clone from github
-git clone https://github.com/williamwanjia/VTK.git VTK_5.10
+git clone https://github.com/williamwanjia/VTK.git vtk_5
 
-cd VTK_5.10
-git checkout v5.10.1
+cd vtk_5
+git checkout v5_fix
+
+cd ../
+
+if [-d "vtk5_build"]; then
+    echo "build folder exists"
+else
+    mkdir vtk5_build
+fi
+
+cd vtk5_build
+
+cmake -Wno-dev -DBUILD_SHARED_LIBS:BOOL=ON \
+      -DVTK_WRAP_TCL:BOOL=ON \
+      -DCMAKE_CXX_FLAGS:STRING=-std=c++03 \
+      -DTCL_INCLUDE_PATH:PATH=/usr/include/tcl8.5 \
+      -DTCL_LIBRARY:FILEPATH=/usr/lib/x86_64-linux-gnu/libtcl8.5.so \
+      -DTCL_TCLSH:FILEPATH=/usr/bin/tclsh8.5 \
+      -DTK_INCLUDE_PATH:PATH=/usr/include/tcl8.5 \
+      -DTK_INTERNAL_PATH:PATH=/home/jiawan/repos/vtk5/Utilities/TclTk/internals/tk8.5 \
+      -DTK_LIBRARY:FILEPATH=/usr/lib/x86_64-linux-gnu/libtk8.5.so \
+      ../vtk5
+
+make -j3
+
+sudo make install
+
 
