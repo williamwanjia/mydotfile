be237c22235d173e69d56ed4156e8360ef70590d
Jia Wan <williamwanjia@gmail.com>
2022-04-02 21:35:15 +0200

    Add nvim file

--- install/install_ros_melodic.sh
+++ install/install_ros_melodic.sh
@@ -1,14 +1,21 @@
 
 set -e
+set -x
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
-sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' \
-    --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt install curl # if you haven't already installed curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
 sudo apt update
 
-sudo apt install -y ros-melodic-desktop-full
+# sudo apt install -y ros-melodic-desktop-full
+
+sudo apt install -y ros-melodic-ros-base
+
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+
+source ~/.bashrc
 
 sudo apt install -y python-rosdep python-rosinstall \
     python-rosinstall-generator python-wstool build-essential
